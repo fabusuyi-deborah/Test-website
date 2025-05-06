8840d845a1e208f1564cd89a562d1281df51c6c5
fabusuyi-deborah <fabusuyideborah03@gmail.com>
2025-05-06 22:58:11 +0100

    made section3 responsive

--- src/components/Sect3.jsx
+++ src/components/Sect3.jsx
@@ -8,9 +8,10 @@ import MT from '../assets/MT.png'
 const Sect3 = () => {
   return (
     <div>
-      <section className=' m-12'>
-        <div className='flex flex-col md:flex-row  bg-[#F1F5F9]  rounded-lg'>
-          <div className=' text-[#1C1C1C] flex flex-col justify-center  md:px-12'>
+      <section className='flex flex-col justify-center items-center py-8 px-4  md:py-8 lg:mx-auto'>
+
+        <div className='flex flex-col md:flex-row  bg-[#F1F5F9]  rounded-lg m-4'>
+          <div className=' text-[#1C1C1C] flex flex-col justify-center  md:px-12 m-6 '>
             <h2 className='text-3xl font-bold mb-4 '>Real-Time Moderation</h2>
             <p className=''>Keep your discussions focused and relevant with live question approval or rejection</p>
           </div>
@@ -22,7 +23,7 @@ const Sect3 = () => {
         <div className='grid grid-cols-1 md:grid-cols-3 gap-8 py-12 px-6'>
 
           <div className='bg-[#FEF3C7] p-4 rounded-2xl' > 
-            <div className='w-[220px]'>
+            <div className=''>
               <img src={EC} alt=""  className='' />
             </div>
             <div className='pt-4'>
@@ -32,7 +33,7 @@ const Sect3 = () => {
           </div>
 
           <div className='bg-[#EAF8FF] p-4 rounded-2xl'>
-            <div className='w-[250px]'>
+            <div className=''>
               <img src={HFD} alt="" className='' />
             </div>
             <div className='pt-4' >
@@ -44,7 +45,7 @@ const Sect3 = () => {
   
 
         <div className='bg-[#FAF8F7] p-4 rounded-2xl'>
-          <div className='w-[240px]'>
+          <div className=''>
             <img src={HFDL} alt="" />
           </div>
           <div className='pt-4'>
@@ -55,8 +56,8 @@ const Sect3 = () => {
           
           </div>
 
-        <div className='flex flex-col md:flex-row  bg-[#EDE9FE] rounded-lg'>
-          <div className=' text-[#1C1C1C]  flex flex-col justify-center md:px-12'>
+        <div className='flex flex-col md:flex-row  bg-[#EDE9FE] rounded-lg m-4'>
+          <div className=' text-[#1C1C1C]  flex flex-col justify-center md:px-12 m-6'>
             <h2 className=' text-3xl font-bold mb-4'>Post-Event Analytics</h2>
             <p>Gain insights into your event’s engagement with metrics on question volume, upvotes, and attendee participation</p>
           </div>
